aaaf92d2ed680f6bad97c17a451b44a566a8b6eb
Carlos Cendejas <carloscb02@outlook.com>
2021-10-06 17:01:07 -0500

    ADDED: server in port 8080

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-require('dotenv').config();
+require('dotenv').config()
+const express = require('express');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { Sequelize } = require('sequelize');
 
-const PORT = process.env.NODE_DOCKER_PORT || process.env.PORT;
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:3000"
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+// const sequelize = new Sequelize(process.env.MYSQLDB_DATABASE, process.env.MYSQLDB_USER, process.env.MYSQLDB_ROOT_PASSWORD, {
+//   host: 'localhost',
+//   dialect: 'mysql'
+// });
